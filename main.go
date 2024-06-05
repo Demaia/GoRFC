fcc64999943dbf2de0027d6ecce34fbb9ed63f38
Laurens van der Stoep <laurens.van.der.stoep@pwc.com>
2024-06-05 15:40:55 +0200

    added funcs and variables

--- main.go
+++ main.go
@@ -25,21 +25,41 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
-	SnowServiceAccountName     string
-	SnowServiceAccountPassword string
-	snowenv                    string
-	template_sys_id            string
-	headers                    map[string]string
+	SnowServiceAccountName     string            = os.Getenv("AUTORFC_SNOWSANAME")
+	SnowServiceAccountPassword string            = os.Getenv("AUTORFC_SNOWSAPASS")
+	snowenv                    string            = os.Getenv("AUTORFC_SNOWENV")
+	template_sys_id            string            = "f7cfa23fdb39421052e652f3f396192f"
+	headers                    map[string]string = map[string]string{
+		"apikey":              os.Getenv("AUTORFC_APIKEY"),
+		"apikeysecret":        os.Getenv("AUTORFC_APIPASS"),
+		"Proxy-Authorization": os.Getenv("AUTORFC_PROXYAUTH"),
+		"Accept":              "application/json",
+		"Content-Type":        "application/json",
+	}
 )
 
 func main() {
+	log.Printf("Service account: 	%s", SnowServiceAccountName)
+	log.Printf("SA password: 		%s", SnowServiceAccountPassword)
+	log.Printf("Environment in use: %s", snowenv)
+	log.Printf("Template in use: 	%s", template_sys_id)
+	for k, v := range headers {
+		log.Printf("%s : %s", k, v)
+	}
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/{}", home)
-
+	mux.HandleFunc("/change/create", createChange)
+	mux.HandleFunc("/change/retrieve", retrieveChangeNo)
+	mux.HandleFunc("/change/implement", implementChange)
+	mux.HandleFunc("/change/close", closeChange)
+	mux.HandleFunc("/change/cancel", cancelChange)
+	mux.HandleFunc("/change/closectask", closeChangeCtask)
 	log.Print("Starting the server at :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
@@ -47,4 +67,26 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Print("Homepage")
+}
+
+func createChange(w http.ResponseWriter, r *http.Request) {
+}
+
+func retrieveChangeNo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func implementChange(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func closeChange(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func cancelChange(w http.ResponseWriter, r *http.Request) {
+
+}
+func closeChangeCtask(w http.ResponseWriter, r *http.Request) {
+
 }
